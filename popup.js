2017270d4b00fda34c5e5ed221a551e0b3d6ff13
Anshuman Mund <anssshu@gmail.com>
2015-04-07 23:37:55 +0530

    auto popup killing

--- popup.js
+++ popup.js
@@ -48,7 +48,7 @@ var onDownloadClick=function(){
     var txt=''
     for (var i=0;i<total;i++) {
         if (w.localStorage[total-1-i] !="") txt +=w.localStorage[total-1-i]+"\n\n##############################################################################################################\n";}
-    var dnl_file = new Date().toDateString()+"_data.doc"
+    var dnl_file = new Date().toDateString()+"_data.doc";
     //set the name of the download  file
     document.getElementById('link').setAttribute("download",dnl_file);
     document.getElementById('link').onclick=function(code){
